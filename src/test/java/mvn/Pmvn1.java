74aa611f07b4f9004944bf44ec81804ebd3d4368
Sharan48 <Sharanu318@gmail.com>
2022-07-30 20:06:13 +0530

    Maven Practice

---
+++ src/test/java/mvn/Pmvn1.java
@@ -0,0 +1,16 @@
+package mvn;
+
+import org.testng.annotations.Test;
+
+public class Pmvn1 {
+@Test
+public void pmn() {
+	System.out.println("mvn practice");
+}
+
+@Test
+public void pmn1() {
+	System.out.println("mvn practice");
+}
+
+}
